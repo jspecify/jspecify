2f3fa10f40b4f1214b8f37d4182568b410dcefab
Chris Povirk <cpovirk@google.com>
2020-05-27 17:45:34 -0400

    Set copyright author in docs to "The jspecify Authors"

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'jspecify'
-copyright = '2020, jspecify'
+copyright = '2020, The jspecify Authors'
 author = 'jspecify'
 
 # The version info for the project you're documenting, acts as replacement for
