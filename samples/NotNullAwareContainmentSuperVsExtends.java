3da1c5aaa63a893736cb8804081a3503c8bcb7ed
Chris Povirk <cpovirk@google.com>
2020-09-18 14:02:34 -0400

    Sample for non-@NullAware/non-@DefaultNonNull containment.

---
+++ samples/NotNullAwareContainmentSuperVsExtends.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 The jspecify Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jspecify.annotations.Nullable;
+import org.jspecify.annotations.NullnessUnspecified;
+
+class NotNullAwareContainmentSuperVsExtends {
+  void x() {
+    // NOT-ENOUGH-INFORMATION
+    new Check<@Nullable Lib<? extends Object>, @Nullable Lib<? super Foo>>();
+
+    // NOT-ENOUGH-INFORMATION
+    new Check<@Nullable Lib<? extends @NullnessUnspecified Object>, @Nullable Lib<? super Foo>>();
+
+    new Check<@Nullable Lib<? extends @Nullable Object>, @Nullable Lib<? super Foo>>();
+
+    // NOT-ENOUGH-INFORMATION
+    new Check<@Nullable Lib<?>, @Nullable Lib<? super Foo>>();
+  }
+
+  interface Lib<T extends @Nullable Object> {}
+
+  interface Foo {}
+
+  static class Check<F extends @Nullable Object, A extends F> {}
+}
