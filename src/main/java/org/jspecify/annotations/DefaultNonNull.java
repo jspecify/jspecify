093c717411b5faca43327f2b264e4463a9f9f233
Chris Povirk <cpovirk@google.com>
2021-02-08 11:26:29 -0500

    Revert "Change `@DefaultNonNull` to `CLASS` retention."
    Motivation:
    
    1. sdeleuze points out that Spring needs `RUNTIME` retention:
    https://github.com/jspecify/jspecify/issues/28#issuecomment-772644083
    
    2. The problem I'd seen with `RUNTIME` retention is more limited than
    I'd initially feared. The downside of that is that it's still not clear
    when the problem actually does arise. Given that, we're probably best
    served by using `RUNTIME` retention for a while and trying to identify
    any commonalities from failures that we see:
    https://github.com/jspecify/jspecify/issues/28#issuecomment-774317782
    
    This reverts commit 4fc89fd49663263394bfde630509056df10a816b.

--- src/main/java/org/jspecify/annotations/DefaultNonNull.java
+++ src/main/java/org/jspecify/annotations/DefaultNonNull.java
@@ -17,7 +17,7 @@ package org.jspecify.annotations;
 
 import static java.lang.annotation.ElementType.PACKAGE;
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.CLASS;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -33,5 +33,5 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Target({TYPE, PACKAGE})
-@Retention(CLASS)
+@Retention(RUNTIME)
 public @interface DefaultNonNull {}
