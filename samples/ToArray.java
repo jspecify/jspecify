2cd05793736da61140c7237359c5a0129c634434
Chris Povirk <cpovirk@google.com>
2021-04-21 15:13:57 -0400

    Samples for the no-arg toArray().

---
+++ samples/ToArray.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2021 The JSpecify Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Collection;
+import org.jspecify.nullness.NullMarked;
+import org.jspecify.nullness.Nullable;
+import org.jspecify.nullness.NullnessUnspecified;
+
+@NullMarked
+class ToArray {
+  Object[] wildcard(Collection<?> c) {
+    // jspecify_nullness_mismatch
+    return c.toArray();
+  }
+
+  Object[] object(Collection<Object> c) {
+    return c.toArray();
+  }
+
+  Object[] objectUnspec(Collection<@NullnessUnspecified Object> c) {
+    // jspecify_nullness_not_enough_information
+    return c.toArray();
+  }
+
+  Object[] objectUnionNull(Collection<@Nullable Object> c) {
+    // jspecify_nullness_mismatch
+    return c.toArray();
+  }
+
+  <T> Object[] objectBounded(Collection<T> c) {
+    return c.toArray();
+  }
+
+  <T> Object[] objectBoundedUnspec(Collection<@NullnessUnspecified T> c) {
+    // jspecify_nullness_not_enough_information
+    return c.toArray();
+  }
+
+  <T> Object[] objectBoundedUnionNull(Collection<@Nullable T> c) {
+    // jspecify_nullness_mismatch
+    return c.toArray();
+  }
+
+  <T extends @NullnessUnspecified Object> Object[] unspecBounded(Collection<T> c) {
+    // jspecify_nullness_not_enough_information
+    return c.toArray();
+  }
+
+  <T extends @NullnessUnspecified Object> Object[] unspecBoundedUnspec(
+      Collection<@NullnessUnspecified T> c) {
+    // jspecify_nullness_not_enough_information
+    return c.toArray();
+  }
+
+  <T extends @NullnessUnspecified Object> Object[] unspecBoundedUnionNull(
+      Collection<@Nullable T> c) {
+    // jspecify_nullness_mismatch
+    return c.toArray();
+  }
+
+  <T extends @Nullable Object> Object[] unbounded(Collection<T> c) {
+    // jspecify_nullness_mismatch
+    return c.toArray();
+  }
+
+  <T extends @Nullable Object> Object[] unboundedUnspec(Collection<@NullnessUnspecified T> c) {
+    // jspecify_nullness_mismatch
+    return c.toArray();
+  }
+
+  <T extends @Nullable Object> Object[] unboundedUnionNull(Collection<@Nullable T> c) {
+    // jspecify_nullness_mismatch
+    return c.toArray();
+  }
+
+  Object[] fromClassTypeParameterBound(CollectionWithObjectBound<?> c) {
+    return c.toArray();
+  }
+
+  interface CollectionWithObjectBound<T> extends Collection<T> {
+    default Object[] toArrayOfNonNull() {
+      return toArray();
+    }
+  }
+
+  interface CollectionWithUnspectBound<T extends @NullnessUnspecified Object>
+      extends Collection<T> {
+    default Object[] toArrayOfNonNull() {
+      // jspecify_nullness_not_enough_information
+      return toArray();
+    }
+  }
+
+  interface CollectionWithNoBound<T extends @Nullable Object> extends Collection<T> {
+    default Object[] toArrayOfNonNull() {
+      // jspecify_nullness_mismatch
+      return toArray();
+    }
+  }
+}
