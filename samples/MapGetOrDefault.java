f74db28172b1f05b0f6722aa8a10251091284cf8
Chris Povirk <cpovirk@google.com>
2021-06-28 10:02:34 -0400

    Samples for `Map.getOrDefault`.
    We should someday extend these to include samples for non-`@NullMarked`
    code, but this is a start.

---
+++ samples/MapGetOrDefault.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 The JSpecify Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Map;
+import org.jspecify.nullness.NullMarked;
+import org.jspecify.nullness.Nullable;
+
+@NullMarked
+class MapGetOrDefault {
+  Object nonnullValuesNonnullDefault(Map<Object, Object> map, Object key, Object defaultValue) {
+    return map.getOrDefault(key, defaultValue);
+  }
+
+  Object nonnullValuesNullDefault(
+      Map<Object, Object> map, Object key, @Nullable Object defaultValue) {
+    // jspecify_nullness_mismatch
+    return map.getOrDefault(key, defaultValue);
+  }
+
+  Object nullValuesNonnullDefault(
+      Map<Object, @Nullable Object> map, Object key, Object defaultValue) {
+    // jspecify_nullness_mismatch
+    return map.getOrDefault(key, defaultValue);
+  }
+
+  Object nullValuesNullDefault(
+      Map<Object, @Nullable Object> map, Object key, @Nullable Object defaultValue) {
+    // jspecify_nullness_mismatch
+    return map.getOrDefault(key, defaultValue);
+  }
+}
