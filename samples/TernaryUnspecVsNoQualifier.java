d7ef46626494cdc2aa5fcb8d98929cae110c4430
Chris Povirk <cpovirk@google.com>
2021-07-23 13:55:05 -0400

    Sample that involves comparing type arguments that are type variables, one of which comes from "annotated" code and one of which comes from "unannotated" code.
    Our prototype checker currently crashes on this input.

---
+++ samples/TernaryUnspecVsNoQualifier.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2021 The JSpecify Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Collections;
+import org.jspecify.nullness.NullMarked;
+
+/*
+ * I haven't thought deeply about where checkers could identify mismatches or not-enough-information
+ * findings in this class: I wrote it mainly to reproduce a crash, and I updated our prototype
+ * checker with the goal of avoiding that crash, not fully handling the situation correctly.
+ */
+class TernaryUnspecVsNoQualifier {
+  @NullMarked
+  static class ImmutableList<E> implements Iterable<E> {
+    static <E> ImmutableList<E> of(E e) {
+      return new ImmutableList<>();
+    }
+
+    @Override
+    public java.util.Iterator<E> iterator() {
+      return Collections.<E>emptyList().iterator();
+    }
+  }
+
+  static class Iterables {
+    static <E> Iterable<E> concat(Iterable<? extends E> a, Iterable<? extends E> b) {
+      // jspecify_nullness_not_enough_information
+      return Collections.emptyList();
+    }
+  }
+
+  abstract static class Foo<T> {
+    abstract Iterable<Foo<T>> others();
+
+    void go(boolean b) {
+      // jspecify_nullness_not_enough_information
+      Iterable<Foo<T>> it =
+          // jspecify_nullness_not_enough_information
+          b ? ImmutableList.of(this) : Iterables.concat(others(), ImmutableList.of(this));
+    }
+  }
+}
