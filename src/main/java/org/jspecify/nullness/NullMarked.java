a2b527a93e927e1f1f87ed8327638d2fb8ec8053
David P. Baker <dpb@google.com>
2022-08-04 16:43:58 -0400

    Fix inadvertent curly quotes.

--- src/main/java/org/jspecify/nullness/NullMarked.java
+++ src/main/java/org/jspecify/nullness/NullMarked.java
@@ -28,7 +28,7 @@ import java.lang.annotation.Target;
 
 /**
  * Indicates that the annotated element and the code transitively <a
- * href=”https://docs.oracle.com/en/java/javase/18/docs/api/java.compiler/javax/lang/model/element/Element.html#getEnclosedElements()”>enclosed</a>
+ * href="https://docs.oracle.com/en/java/javase/18/docs/api/java.compiler/javax/lang/model/element/Element.html#getEnclosedElements()">enclosed</a>
  * within it is <b>null-marked code</b>: type usages are generally considered to exclude {@code
  * null} as a value unless specified otherwise (special cases to be covered below). Using this
  * annotation avoids the need to write {@link NonNull @NonNull} many times throughout your code.
