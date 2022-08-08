dd2f37076db1fa1b003e5a10da500ec96866d2bb
David P. Baker <dpb@google.com>
2022-08-08 15:58:41 -0400

    Change `@Implies` from a repeatable annotation to one with an array of values.

--- src/main/java/org/jspecify/meta/Implies.java
+++ src/main/java/org/jspecify/meta/Implies.java
@@ -20,40 +20,23 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Annotation;
 import java.lang.annotation.Documented;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
  * Indicates that one annotation conveys (at least) all of the same meaning as some other
- * annotation; for example, {@code @Implies(One.class) @interface Two {}} says that any occurrence
+ * annotations; for example, {@code @Implies(One.class) @interface Two {}} says that any occurrence
  * of {@code @Two} implicitly conveys all of the meaning that {@code @One} would in that same
  * position. Note that the {@code Two} annotation type may define additional meaning of its own as
- * well, perhaps even adding additional usages of {@code @Implies}.
+ * well.
  *
  * <p><b>WARNING:</b> This annotation is under development, and <i>any</i> aspect of its naming,
  * location, or design may change before 1.0. <b>Do not release libraries using this annotation at
  * this time.</b>
  */
 @Documented
-@Repeatable(Implies.Container.class)
 @Retention(RUNTIME)
 @Target(ANNOTATION_TYPE)
 public @interface Implies {
-  Class<? extends Annotation> value();
-
-  /**
-   * Container annotation for the repeatable {@link Implies @Implies} annotation. The compiler will
-   * use this to contain repeated applications of {@link Implies}.
-   *
-   * <p><b>WARNING:</b> This annotation is under development, and <i>any</i> aspect of its naming,
-   * location, or design may change before 1.0. <b>Do not release libraries using this annotation at
-   * this time.</b>
-   */
-  @Documented
-  @Retention(RUNTIME)
-  @Target(ANNOTATION_TYPE)
-  @interface Container {
-    Implies[] value();
-  }
+  Class<? extends Annotation>[] value();
 }
