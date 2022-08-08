240a6c4b148585002ad046301adc1d01e53197e6
David P. Baker <dpb@google.com>
2022-08-08 15:00:33 -0400

    Move `Implies` to the `org.jspecify.meta` package.

--- src/main/java/org/jspecify/meta/Implies.java
+++ src/main/java/org/jspecify/meta/Implies.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jspecify.nullness;
+package org.jspecify.meta;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
