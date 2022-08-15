a46c488ff63f2c0bbf3d2bd6fbe3948059bfec70
Artem Pyanykh <arr@fb.com>
2022-08-15 10:54:27 +0100

    Fix a homepage description and a few links (#262)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/jspecify/jspecify/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jspecify/jspecify/tree/main/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/jspecify/jspecify/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jspecify/jspecify/tree/main/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
