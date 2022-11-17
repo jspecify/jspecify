dda81899f11748cedfbf48ad8cdb3c459d25e638
Kevin Bourrillion <kevinb@google.com>
2022-11-17 15:20:46 -0800

    Goodbye, @Implies (#339)
    * Goodbye, @Implies

--- docs/static/docs/api/package-search-index.js
+++ docs/static/docs/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.jspecify.annotations"},{"l":"org.jspecify.meta"},{"l":"org.jspecify.nullness"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.jspecify.annotations"},{"l":"org.jspecify.nullness"}];updateSearchResults();
