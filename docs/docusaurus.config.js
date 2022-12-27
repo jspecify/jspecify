30bf731d94ed9faea9e359fa9ce5915ef3079fe5
Chris Povirk <cpovirk@google.com>
2022-12-27 17:44:49 -0500

    Fix "Draft Javadoc" link.
    Compare https://github.com/jspecify/jspecify/pull/324.
    
    I notice that this makes the link open in a new tab by default, too, as
    presumably is the default for "external" pages. That's not necessarily
    ideal, but it's tolerable, and it's way better than a broken link :)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
               },
               {
                 label: 'Draft Javadoc',
-                href: '/docs/api/org/jspecify/annotations/package-summary.html',
+                href: 'http://jspecify.org/docs/api/org/jspecify/annotations/package-summary.html',
               },
               {
                 label: 'Draft Specification',
