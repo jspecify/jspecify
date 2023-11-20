e4acb7edef0b48b7d3c0c2e105dffd8e802c21ac
David P. Baker <dpb@google.com>
2023-11-20 16:18:33 -0500

    Move conformance test assertions source to `org.jspecify.conformance.tests` package.

--- conformance-tests/src/assertions/java/org/jspecify/conformance/tests/Basic.java
+++ conformance-tests/src/assertions/java/org/jspecify/conformance/tests/Basic.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package basic;
+package org.jspecify.conformance.tests;
 
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
