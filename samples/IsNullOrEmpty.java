afeebe4123b8f3f8a473d707db61498d98c97691
David P. Baker <dpb@google.com>
2024-01-03 13:06:10 -0500

    Remove conflict markers

--- samples/IsNullOrEmpty.java
+++ samples/IsNullOrEmpty.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-<<<<<<<< HEAD:samples/IsNullOrEmpty.java
 import static com.google.common.base.Strings.isNullOrEmpty;
 
 import org.jspecify.annotations.NullMarked;
@@ -23,16 +22,5 @@ import org.jspecify.annotations.Nullable;
 class IsNullOrEmpty {
   Object x(@Nullable String s) {
     return isNullOrEmpty(s) ? "" : s;
-========
-package org.jspecify.conformance.tests;
-
-import org.jspecify.conformance.deps.Dep;
-
-/** This is here only to prove that assertions can refer to classes in deps. */
-public class UsesDep {
-  public static Dep dep() {
-    // test:cannot-convert:null? to Dep*
-    return null;
->>>>>>>> 8f1c5091a1cc9cd6f2cc26a0f884394d430ae06f:conformance-tests/src/assertions/java/org/jspecify/conformance/tests/UsesDep.java
   }
 }
